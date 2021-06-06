0ae976c0062b766e3a84b6d03db013291cf12c3c
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-06-06 18:36:13 +0300

    commit files

--- apiproject/src/App.js
+++ apiproject/src/App.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react'
 import Makeup from './Makeup'
+import axios from 'axios'
+import Title from './Title';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      images: [],
+      price:[],
+      description: []
+    }
+  }
+  componentDidMount() {
+    this.callApi()
+  }
+
+  callApi() {
+    axios({
+      method: "get",
+      url: 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+    }).then((Albandry) => {
+      console.log("res", Albandry.data);
+
+      const data = Albandry.data;
+      const img = Albandry.images;
+      const price = Albandry.price;
+      const description = Albandry.description;
+
+      this.setState({ data, img, price,description });
+
+    }).catch((e) => {
+      console.log("error", e);
+    })
+  }
+
+
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  componentDidUpdate(){
+    alert("Welcome in our Makeup Magazine, have a nice reading (:")
+
+  }
+
   render() {
     return (
       <div>
-        <Makeup/>
+        <Title/>
+         {this.state.data.map(p =>
+              
+        <Makeup 
+        name={p.name}
+        image_link={p.image_link}
+        price={p.price}
+        description={p.description}/>
+
+         )}
       </div>
     )
   }
--- apiproject/src/Makeup.js
+++ apiproject/src/Makeup.js
@@ -1,67 +1,20 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
-export default class Product extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          data: [],
-          images: [],
-          price:[],
-          description: []
-        }
-      }
-      componentDidMount() {
-        this.callApi()
-      }
-    
-      callApi() {
-        axios({
-          method: "get",
-          url: 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
-        }).then((Albandry) => {
-          console.log("res", Albandry.data);
-    
-          const data = Albandry.data;
-          const img = Albandry.images;
-          const price = Albandry.price;
-          const description = Albandry.description;
-    
-          this.setState({ data, img, price,description });
-    
-        }).catch((e) => {
-          console.log("error", e);
-        })
-      }
-    
-    
-      shouldComponentUpdate() {
-        return true;
-      }
 
-      componentDidUpdate(){
-        alert("Welcome in our Makeup Magazine, have a nice reading (:")
-
-      }
+export default class Makeup extends Component {
   
     
       render() {
         return (
           <div className="tb">
-            <h1>Makeup Magazine</h1>
-              {this.state.data.map(p =>
-              
+        
               <div className="my">
-                <p className="name"> {p.name}</p>
-              <img src={p.image_link} height="200px"></img>
-              <p>Price: {p.price}</p>
-              <p className="des">{p.description}</p>
+                <p className="name"> {this.props.name}</p>
+              <img src={this.props.image_link} height="200px"></img>
+              <p>Price: {this.props.price}</p>
+              <p className="des">{this.props.description}</p>
               </div>
-    
-           
-            
-              )}
-    
+
           </div>
         )
   
