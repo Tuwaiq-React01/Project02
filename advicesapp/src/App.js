be34c1996623c7a70c61bcbedca062c9cd43fbae
Sara Abdulmajeed <Sara.Alnasser@tuwaiq.edu.sa>
2021-06-07 02:00:26 +0300

    FirstVersion

---
+++ advicesapp/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import axios from "axios"
+import NavigateBar from './Component/NavigateBar';
+import Body from './Component/Body';
+import './App.css'
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      advice:""
+    }
+
+  }
+  componentDidMount(){
+    this.getAdvice();
+  }
+
+  shouldComponentUpdate() {
+    return true;
+  }
+ 
+
+  getAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+ 
+    
+  
+    // }
+  render() {
+    return (
+      <div className="App" >
+        <center >
+        <h1 style={{margin:'150px',marginBottom:"-50px",  textShadow: "0 0 5px #FFFF00"}}>API To Generate an Advice To You </h1>
+        <div className="card" style={{padding:"50px",margin:'150px',marginTop:"105px",boxShadow: "5px 10px #888888"}}>
+       <h5 style={{paddingBottom:"45px"}}>{this.state.advice}</h5> 
+       <button style = {{margin:"25px",padding:"-250 statpx"}}className="btn btn-primary" onClick={this.getAdvice}>New Advice</button>
+       </div>
+       </center>
+       </div>
+    )
+  }
+}
---
+++ advicesapp/src/Component/NavigateBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default class NavigateBar extends Component {
+    render() {
+        return (
+         
+          <nav class="navbar navbar-light bg-light">
+          <div class="container">
+            <a class="navbar-brand" >
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYhUxfcxJMOE--GlwDPMSe0jukK83j816W2QyGFhZN62uX69ve5c3RIukNPxGAYDE9TIA&usqp=CAU" alt="" width="30" height="24"/>
+            </a>
+          </div>
+        </nav>
+        )
+    }
+}
